917197c924a76213a6e70f9091823f4cafab4c79
Mirabel Ekwenugo <mirabel.ekwenugo@andela.co>
2015-09-18 15:02:11 +0100

    added a filter to the html page based on the cuisine name

--- restaurants/views.py
+++ restaurants/views.py
@@ -8,5 +8,12 @@ class RestaurantListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(RestaurantListView, self).get_context_data(**kwargs)
-        # context['now'] = timezone.now()
+        context['cuisine_list'] = restaurants_models.Cuisine.objects.all()
         return context
+
+    def get_queryset(self):
+        queryset = super(RestaurantListView, self).get_queryset()
+        cuisine_name = self.request.GET.get('cuisine')
+        if cuisine_name:
+            queryset = queryset.filter(cuisines__name=cuisine_name)
+        return queryset
