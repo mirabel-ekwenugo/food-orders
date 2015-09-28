5f467fd97f947d049a81c2acddc27308a21fe6d9
Mirabel Ekwenugo <mirabel.ekwenugo@andela.co>
2015-09-28 15:18:03 +0100

    setup amazon s3 for storing media files

--- food_orders/settings.py
+++ food_orders/settings.py
@@ -95,6 +95,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_STORAGE_BUCKET_NAME = 'food-orders'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', 'AKIAIWBBIAKMTD5F3E5A')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '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')
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
@@ -103,5 +112,10 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/tmp/restaurants/media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/tmp/restaurants/media'
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
